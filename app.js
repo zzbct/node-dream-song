7967c42ca3e3380f2ef8e061802f5d1446982475
zz bct <1668856072@qq.com>
2017-04-10 19:26:36 +0800

    爬取-单个歌单信息-歌曲列表

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ var robot = new Robot(op);
 
 //获取个性推荐的img数据
 app.get('/img',function(req,resq){
-  var path = '/discover';
+  var path = 'http://music.163.com/discover';
   var p = new Promise(function(resolve,reject){
     robot.go(path,'script',resolve);
   })
@@ -58,7 +58,7 @@ app.get('/img',function(req,resq){
   })
 })
 
-//获取歌单数据
+//获取歌单列表
 app.get('/songList',function(req,resq){
   var reqData = [];
   var path = '/discover/playlist';
@@ -71,17 +71,35 @@ app.get('/songList',function(req,resq){
      data.each(function() {
        var cap = $(this);
        var item = {
-         src: cap.find('img.j-flag').attr('src'),
-         title: cap.find('a.msk').attr('title'),
-         id: cap.find('a.f-fr').attr('data-res-id'),
-         type: cap.find('a.f-fr').attr('data-res-type'),
-         nb: cap.find('span.nb').text(),
-         creater: cap.find('a.s-fc3').text(),
-         byhref: cap.find('a.s-fc3').attr('href')
+         src: cap.find('img.j-flag').attr('src'),//图片路径
+         title: cap.find('a.msk').attr('title'),//歌单名称
+         id: cap.find('a.f-fr').attr('data-res-id'),//歌单id
+         type: cap.find('a.f-fr').attr('data-res-type'),//歌单种类
+         nb: cap.find('span.nb').text(),//歌单播放量
+         creater: cap.find('a.s-fc3').text(),//歌单发行者
+         byhref: cap.find('a.s-fc3').attr('href'),//发行者地址
+         hasStar: cap.find('sup.u-icn-84').length,
+         hasWy: cap.find('sup.u-icn2-music2').length,
+         hasV: cap.find('sup.u-icn-1').length
       };
       itemList.push(item);
     })
-    resq.send(itemList);
+    resq.json(itemList);
+  })
+})
+
+//获取单个歌单信息
+app.get('/secondList',function(req,resq){
+  var reqData = [];
+  var context = '#song-list-pre-cache';
+  var params = req.query;
+  var path = '/playlist?id=' + params.id;
+  var p = new Promise(function(resolve,reject){
+    robot.go(path,'#song-list-pre-cache textarea',resolve);
+  })
+  p.then(function(data){
+    var result = eval('('+data.text()+')');
+    resq.send(result);
   })
 })
 
